790b28e59bcdab33464da2b038d2077aed2490f4
tishka <alex.gromov2@mail.ru>
2026-08-30 01:57:04 +0300

    fix: cli pseudo seed handle

--- cmd/passgen/main.go
+++ cmd/passgen/main.go
@@ -15,9 +15,9 @@ var (
 	count = flag.Int64("count", 1, "count of generation; default: 1")
 )
 
-func GeneratePseudoCLI(length uint64, seed int64, source string) string {
-	rsp := passgen.NewSourcePseudo(seed, source)
-	return rsp.Generate(length)
+
+func pseudoSource(seed int64, source string) passgen.PseudoGenerate {
+	return passgen.NewSourcePseudo(seed, source)
 }
 
 func GenerateCryptoCLI(length uint64, source string) string {
@@ -33,13 +33,14 @@ func main() {
 	}
 	switch *mode {
 		case 0:
+			pseudoSource := pseudoSource(*seed, *source)
 			if *count > 1 {
 				for range *count {
-					pass := GeneratePseudoCLI(*length, *seed, *source)
+					pass := pseudoSource.Generate(*length)
 					fmt.Println(pass)
 				}
 			} else {
-				pass := GeneratePseudoCLI(*length, *seed, *source)
+				pass := pseudoSource.Generate(*length)
 				fmt.Println(pass)
 			}
 		case 1:
